eff95741e2a4818da8905d2480c808950e688132
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-05-16 09:42:02 +0200

    debut objet Question et Quizzz

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,1 +1,33 @@
+function Quizz() {
+    this.question = [];
+
+    this.addQuestion = function (question) {
+        this.question.push(question);
+    };
+}
+
+function Question(title, answers, goodAnswer) {
+    this.title = title;
+    this.answers = answers;
+    this.goodAnswer = goodAnswer;
+
+    this.getBodyQuestion = function () {
+        let bodyQuestion = this.title;
+        for (let index = 0; index < this.answers.length; index++) {
+            bodyQuestion += '   ' + (index + 1) + ' .. ' + this.answers[index];
+        }
+
+        return bodyQuestion;
+    };
+
+    this.isGoodAnswer = function (answerUser) {
+        if (answerUser === this.goodAnswer) {
+            return true;
+        }
+        return false;
+    };
+}
+
+export { Quizz, Question };
+
 
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,23 +1,43 @@
 // !!!!!!!!!!!!!! pour déclarer des imports / export, il faut typer la balise script du html type="module"
 
-
-
-
-
 //  ============ GROUPER EXPORT IMPORT ================
-
 
 // debut fichier import {sdfqsd, sdfsd,fsdf} from './ssdfd.js'
 
 // fin du fichier export {qsdf,sdfsd,qsdqsd}
 
-
 // ============= IMPORT EXPORT TOUT DS UN OBJET =========
-
-
 
 // import * as Objet from './sfsd.js'
 
-
 // Objet.méthodeImportéé()
 
+import { Quizz, Question } from './functions.js';
+
+let quizzFarfelu = new Quizz();
+
+let question1 = new Question(
+    "Quelle est la couleur du cheval blanc d'Henri 4 ?",
+    ['Rouge', 'Jaune', 'Blanc'],
+    '2'
+);
+
+let question2 = new Question(
+    "Quelle est la couleur du cheval blanc d'Henri 4 ?",
+    ['Rouge', 'Vert', 'Blanc'],
+    '2'
+);
+
+let question3 = new Question(
+    "Quelle est la couleur du cheval blanc d'Henri 4 ?",
+    ['Rouge', 'Orange', 'Blanc'],
+    '2'
+);
+
+quizzFarfelu.addQuestion(question1);
+quizzFarfelu.addQuestion(question2);
+quizzFarfelu.addQuestion(question3);
+
+console.log(question1.getBodyQuestion());
+console.log(question2.getBodyQuestion());
+console.log(question3.getBodyQuestion());
